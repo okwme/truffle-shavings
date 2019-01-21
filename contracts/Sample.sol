3cd16ef46fd983ec7019346cc62dc39838ab1167
Billy Rennekamp <billy.rennekamp@gmail.com>
2019-01-21 15:02:05 +0100

    moved to 5.0

--- contracts/Sample.sol
+++ contracts/Sample.sol
@@ -1,8 +1,8 @@
-pragma solidity ^0.4.20;
+pragma solidity ^0.5.0;
 
 /**
  * The Sample contract does this and that...
  */
 contract Sample {
-    constructor () public {}  
+    constructor() public {}
 }
